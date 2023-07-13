398b101b1035d954f948cc267f5427655ba2917b
Eder Sena <eder.sena@live.com>
2023-07-13 16:53:04 -0300

    chore: get GameById

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.DTO.GameMinDTO;
 // import com.devsuperior.dslist.entitities.Game;
 import com.devsuperior.dslist.services.GameService;
@@ -27,6 +29,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAllMin();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
         return result;
     }
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.DTO.GameMinDTO;
 import com.devsuperior.dslist.entitities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -16,14 +18,27 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        // if the id does not exist, the findById method will return null
+        // the get() method will throw an exception if the id does not exist
+        Game entityResult = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(entityResult);    
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAllMin() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
 
+    @Transactional(readOnly = true)
     public List<Game> findAll() {
         List<Game> result = gameRepository.findAll();
         return result;
     }
+
+
 }
